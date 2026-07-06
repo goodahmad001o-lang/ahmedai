62e346174a6f55d015b7019003816ea047b0907d
MR AHMAD <goodahmad001o@gmail.com>
2026-07-06 14:08:54 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,16 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "AHMEDAI",
+  description: "هوش مصنوعی فارسی AHMEDAI",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
